e158e80efad93c56f33777695b747f4c22d5d53b
differui <differui@gmail.com>
2020-01-29 15:58:34 +0800

    :rocket: change fallback lang

--- src/i18n.ts
+++ src/i18n.ts
@@ -24,7 +24,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    fallbackLng: 'en',
+    fallbackLng: 'zh-CN',
     debug: false,
     resources: {
       en: {
