99c732310e538612a08c103a81f258cabb577352
differui <differui@gmail.com>
2020-01-30 08:45:18 +0800

    :rocket: update api addr

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -4,4 +4,4 @@ export const BASENAME =
     : 'how-many-confirmed-infections-in-wenzhou';
 
 export const URL_ISAACLIN =
-  'http://lab.isaaclin.cn/nCoV/api/area?province=浙江省&latest=0';
+  'https://lab.isaaclin.cn/nCoV/api/area?province=浙江省&latest=0';
